0381c722b1f1865a5f8029546ca33a489cc25c12
bhanusankhyan <bhanusankhyan77@gmail.com>
2018-04-28 11:47:34 +0530

    updates

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime, Time, BigInteger, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -13,7 +13,9 @@ import random
 values = [0,1,2,3,4,5,6,7,8,9]
 class Register(Base):
     __tablename__ = 'register'
-    id = Column(Integer, autoincrement= 10, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable = False)
+    phone = Column(BigInteger, nullable=False, unique=True)
     email = Column(String(30), nullable=False, unique=True)
     password = Column(String(30), nullable=False)
     date =  Column(DateTime, server_default = func.now() )
@@ -31,9 +33,57 @@ class Companies(Base):
     __tablename__ = 'companies'
     id = Column(Integer, primary_key = True, autoincrement = 20)
     name = Column(String(20), nullable = False, unique = True)
-    basic = Column(Integer, nullable = False)
-    standard = Column(Integer, nullable = False)
-    comprehensive = Column(Integer, nullable = False)
 
-engine = create_engine('postgresql+psycopg2://bhanu:bhanu@localhost/experiment6')
+class Models(Base):
+    __tablename__  = 'models'
+    id = Column(Integer, ForeignKey('companies.id'))
+    model_id = Column(Integer, autoincrement = True)
+    company_id = relationship(Companies)
+    model = Column(String(20), nullable=False, unique = True, primary_key = True)
+    basic = Column(Integer)
+    standard = Column(Integer)
+    comprehensive = Column(Integer)
+
+    @property
+    def serial(self):
+      return{
+           'model' : self.model,
+           'basic' : self.basic,
+           'standard' : self.standard,
+           'comprehensive' : self.comprehensive,
+  }
+
+class Booking(Base):
+    __tablename__ = 'bookings'
+    id = Column(Integer, nullable=False, autoincrement = True, primary_key=True)
+    user_id = Column(Integer, ForeignKey('register.id'))
+    reigster = relationship(Register)
+    model = Column(String(20),nullable=False)
+    workshop_id = Column(Integer)
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    companies = relationship(Companies)
+    booking_type = Column(String(30),nullable= False)
+    price = Column(Integer)
+    booking_time = Column(DateTime, server_default = func.now())
+    booking_date = Column(Date, nullable = False)
+    time_slot1 = Column(Time, nullable=False)
+    time_slot2 = Column(Time)
+
+class Workshop(Base):
+    __tablename__ = 'workshops'
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    date = Column(DateTime, server_default = func.now())
+    name = Column(String(40), nullable= False)
+    email_address = Column(String(30), nullable=False, unique=True)
+    phone_number = Column(BigInteger,nullable=False,unique=True)
+    latitude = Column(String(40))
+    longitude = Column(String(40))
+    password = Column(String(30))
+    aadhar_num = Column(BigInteger,nullable=False,unique=True)
+    workshop_id = Column(String(30),unique=True, nullable=False)
+    gst = Column(String(30),nullable = False, unique=True)
+
+
+
+engine = create_engine('postgresql+psycopg2://bhanu:bhanu@localhost/mendax')
 Base.metadata.create_all(engine)
--- sample.py
+++ sample.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, flash, jsonify, url_for, redi
 from flask import session as login_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from database_setup import Base, Register
+from database_setup import Base, Register, Models, Companies, Booking, Workshop
+from sqlalchemy.sql import text
 import json
-engine =  create_engine("postgresql+psycopg2://bhanu:bhanu@localhost/experiment6")
+from sqlalchemy.orm import load_only
+engine =  create_engine("postgresql+psycopg2://bhanu:bhanu@localhost/mendax")
 Base.metadata.bind = engine
 DBsession = sessionmaker(bind=engine)
 session = DBsession()
@@ -17,9 +19,15 @@ app = Flask(__name__)
 def HomePage():
     return render_template('home.html')
 
-@app.route('/register', methods=['POST','GET'])
+@app.route('/register')
 def RegisterSuccess():
 
+    if request.method == 'GET':
+        return render_template('register1.html')
+
+
+@app.route('/user_register',methods=['GET','POST'])
+def userRegister():
     if request.method == 'GET':
         return render_template('register.html')
 
@@ -30,7 +38,7 @@ def RegisterSuccess():
                 flash('Password does not match')
                 return redirect('/register')
             if user == None:
-                newuser = Register(email = request.form['email'], password = request.form['pass'])
+                newuser = Register(name=request.form['name'],phone=request.form['phone'],email = request.form['email'], password = request.form['pass'])
                 session.add(newuser)
                 flash ('User Created Succcessfully')
                 flash('You Can Now Login')
@@ -46,6 +54,23 @@ def RegisterSuccess():
             return redirect("/register")
 
 
+
+
+
+
+@app.route('/workshop_register', methods=['POST','GET'])
+def workshopRegister():
+    if request.method == "GET":
+        return render_template('workshop_register.html')
+    if request.method == 'POST':
+        newWorkshop = Workshop(name =request.form['name'], email_address =request.form['email'] , phone_number =request.form['phone'] ,password = request.form['pass'], aadhar_num = request.form['aadhar_number'],workshop_id = request.form['workshop_id'], gst = request.form['gst_number'] )
+        session.add(newWorkshop)
+        session.commit()
+        flash("New Workshop Created")
+        return redirect('/')
+
+
+
 @app.route('/register/jsonapi', methods = ['POST','GET'])
 def RegisterAPI():
     if request.method == 'GET':
@@ -53,23 +78,27 @@ def RegisterAPI():
         return jsonify(users=[u.serialize for u in json])
     elif request.method == 'POST':
 
-        try:
+        #try:
 
             data = request.get_json()
-            newuser = Register(email = data['email'], password = data['password'])
+            newuser = Register(email = data['email'], password = data['password'],phone=data['phone'], name=data['name'])
             session.add(newuser)
             session.commit()
             return jsonify({"result" : "!success"})
-        except:
-            session.rollback()
-            return jsonify({'result' : '!failure'})
+        #except:
+            #session.rollback()
+            #return jsonify({'result' : '!failure'})
 
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login')
 def Login():
+        return render_template('login1.html')
+        #user = session.query(Register).filter_by(email=request.form['email'],password = request.form['pass']).first()
+
+@app.route('/user_login',methods=['POST','GET'])
+def userLogin():
     if request.method == 'GET':
         return render_template('login.html')
     if request.method == 'POST':
-        #user = session.query(Register).filter_by(email=request.form['email'],password = request.form['pass']).first()
         try:
             user = session.query(Register).filter_by(email=request.form['email']).one()
             if user.email == request.form['email'] and user.password == request.form['pass']:
@@ -85,24 +114,119 @@ def Login():
         except:
             flash('Invalid credentials! Please Register')
             return redirect('/register')
-@app.route('/backgroud_process')
-def backgroundProcess():
-    try:
-        model = request.args.get('carModel')
-        return jsonify(result = 'Hell')
-    except Exception(e):
-        return(str(e))
 
+@app.route('/workshop_login',methods=['POST','GET'])
+def workshopLogin():
+    if request.method == 'GET':
+        return render_template('workshop_login.html')
+    if request.method == 'POST':
+        try:
+            user = session.query(Workshop).filter_by(email_address = request.form['email']).one()
+            if user.email_address == request.form['email'] and user.password == request.form['pass']:
+                flash('Yor are successfully logged into your Workshop')
+                login_session['workshop']=request.form['email']
+                return redirect('/')
+        except:
+            flash('Invalid Credentials')
+            return redirect('/workshop_login')
+
+
+@app.route('/background_process', methods=['POST'])
+def backgroundProcess():
+    #try:
+        #car = request.args.get(model)
+        carModel = request.get_data().decode('utf-8')
+        data = json.loads(carModel)
+        login_session['carmodel'] = data['model']
+        #data = data['model'].encode('utf-8')
+        que = session.query(Models).filter_by(model=data['model']).one()
+
+        #payload = []
+        #for result in que:
+        #    content = {'model':que.model , 'basic' : que.basic, 'standard' : que.standard, 'comprehensive' : que.coprehensive}
+        #    payload.append(content)
+        #    content ={}
+        return jsonify(model = que.model, basic = que.basic, standard = que.standard, comprehensive = que.comprehensive)
+    #except:
+        #return jsonify(result = "not successful"
+
+@app.route('/schedule_service',methods=['POST'])
+def scheduleService():
+    #try:
+      carmodel = login_session['carmodel']
+      service_type = request.form['service']
+      date_time = request.form['s_date']
+      date, time = date_time.split(" ")
+      user = session.query(Register).filter_by(email=login_session['username']).one()
+      model = session.query(Models).filter_by(model = carmodel).one()
+      model_id = model.model_id
+      #print(type(model.str(service_type)))
+      company = session.query(Companies).filter_by(id = model.id).one()
+      #price = session.query(Models).filter_by(model = carmodel).options(load_only(*['comprehensive'])).first()
+      price = session.execute("select {:s} from models where model_id = {:d}".format(service_type,model_id)).first()
+      cost = price[0]
+      print(cost)
+      print(company.name)
+      booking = Booking(user_id= user.id, company_id = company.id,model=carmodel, booking_type = service_type, price = cost, booking_date=date, time_slot1=time)
+      session.add(booking)
+      session.commit()
+      #print(service_type)
+      #print(date_time)
+      #print(carmodel)
+      #print(booking.id)
+      #login_session['booking_id']= booking.id
+      flash('Booking Scheduled')
+      #return redirect('/')
+
+
+      workshops = session.query(Workshop).all()
+      return render_template('select_workshop.html',name = booking.model, date = booking.booking_date, workshops = workshops)
+    #except:
+      # flash("Please login first To Schedule a Booking")
+       #return redirect('/login')
+
+#@app.route('/select_workshop',methods=['POST'])
+#def selectWorkshop():
+
+     # shop = request.get_data().decode('utf-8')
+     # data = json.loads(shop)
+     # print(data['workshop'])
+     # booking = session.query(Booking).filter_by(id = login_session['booking_id']).one()
+     # print(booking)
+     # workshop = session.query(Workshop).filter_by(name = data['workshop']).one()
+     # print (workshop)
+      #booking.workshop_id = workshop.id
+      #session.add(booking)
+      #session.commit()
+      #del login_session['carmodel']
+      #return redirect('/')
+
+@app.route('/_modal_processing', methods=['POST'])
+def modalProcessing():
+    company  = request.get_data().decode('utf-8')
+    data = json.loads(company)
+    print (data)
+    query = session.query(Companies).filter_by(name = data['name']).one()
+    models = session.query(Models).filter_by(id =  query.id).all()
+    print (models)
+    return jsonify(models =  models)
 
 @app.route('/logout')
 def logout():
     del login_session['username']
     flash('You are now logged out')
     return redirect('/')
+
+@app.route('/workshop_logout')
+def workshopLogout():
+    del login_session['workshop']
+    flash('Logged Out From you Workshop')
+    return redirect('/')
 @app.route('/profile')
 def UserProfile():
-    return render_template('profile.html')
-
+    user  = session.query(Register).filter_by(email=login_session['username']).one()
+    booking = session.query(Booking).filter_by(id = user.id).all()
+    return render_template('profile.html', name = user.name, email = user.email, phone = user.phone, booking_time = booking.booking_time)
 
 if __name__ =='__main__':
     app.secret_key = 'super_secret_key'
